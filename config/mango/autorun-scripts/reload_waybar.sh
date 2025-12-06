9ab6c3b876faf55bbd00e02775fe4d917bc538c6
Lynndroid21 <jwi.audio@hotmail.com>
2025-12-06 01:09:42 -0500

    Update reload_waybar.sh

--- config/mango/autorun-scripts/reload_waybar.sh
+++ config/mango/autorun-scripts/reload_waybar.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-killall waybar ; sleep 3 ; waybar -c ~/.config/mango/config-left.jsonc -s ~/.config/mango/lynn-style.css & waybar -c ~/.config/mango/config-right.jsonc -s ~/.config/mango/lynn-style.css & waybar -c ~/.config/mango/config.jsonc -s ~/.config/mango/lynn-style.css
+killall waybar ; sleep 3 ; waybar -c ~/.config/waybar/config-left.jsonc -s ~/.config/waybar/lynn-style.css & waybar -c ~/.config/waybar/config-right.jsonc -s ~/.config/waybar/lynn-style.css & waybar -c ~/.config/waybar/config.jsonc -s ~/.config/waybar/lynn-style.css
