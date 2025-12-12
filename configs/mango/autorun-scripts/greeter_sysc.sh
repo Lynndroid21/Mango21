7dceaf1d240509329a6bfb9bedaae23fa21ed5a0
Lynndroid21 <jwi.audio@hotmail.com>
2025-12-12 13:21:32 -0500

    Update greeter_sysc.sh

--- configs/mango/autorun-scripts/greeter_sysc.sh
+++ configs/mango/autorun-scripts/greeter_sysc.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-syscgo -effect beam-text -file ~/assets/Lynndroid21.txt -duration 20
+syscgo -effect beam-text -file ~/.config/mango/assets/Lynndroid21.txt -duration 20
 
 command clear
 
 sleep 2
 
-syscgo -effect fire-text -file ~/assets/Arch21.txt -duration 20
+syscgo -effect fire-text -file ~/.config/mango/assets/Arch21.txt -duration 20
 
 command clear
 
